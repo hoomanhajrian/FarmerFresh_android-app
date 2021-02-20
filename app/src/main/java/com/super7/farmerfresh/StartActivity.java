6472d2815c6aa15b53b7a3f69b98bc65d0209378
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2021-02-20 01:34:12 -0800

    added starting activity

---
+++ app/src/main/java/com/super7/farmerfresh/StartActivity.java
@@ -0,0 +1,34 @@
+package com.super7.farmerfresh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.super7.farmerfresh.ui.login.LoginActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_loginbrowse);
+
+        Button signBtn = (Button) findViewById(R.id.sign_button);
+        signBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button browseBtn = (Button) findViewById(R.id.browse_button);
+        browseBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
