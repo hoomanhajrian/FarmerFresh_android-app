3c9417acee33be433f568b00b0c7e49b0a1963af
navneet1328 <66041521+navneet1328@users.noreply.github.com>
2021-03-28 21:43:05 -0700

    Make the path of Splash Activity correct.

--- app/src/main/java/com/super7/farmerfresh/SplashActivity.java
+++ app/src/main/java/com/super7/farmerfresh/SplashActivity.java
@@ -23,7 +23,7 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent=new Intent(SplashActivity.this, OnboardingActivity.class);
                 startActivity(intent);
                 finish();
             }
